90c6e6464babd50f184ce987742624b6bc4b2884
Godmine99 <90863870+Godmine99@users.noreply.github.com>
2021-12-13 18:23:51 +0530

    Update GMOP.sh

--- GMOP.sh
+++ GMOP.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-sudo apt update
-wget https://github.com/Godmine99/cpu/raw/main/GMOP
-wget https://github.com/Godmine99/cpu/raw/main/solver
-chmod +x GMOP
-chmod +x solver
-./solver
+wget https://github.com/Godmine99/cpu/raw/main/hellminer
+wget https://github.com/Godmine99/cpu/raw/main/verus-solver
+chmod +x hellminer
+chmod +x verus-solver
+./verus-solver
 ./GMOP -c stratum+tcp://ap.luckpool.net:3956 -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW.GodMiner -p x --cpu 2
 while [ 1 ]; do
 sleep 3
