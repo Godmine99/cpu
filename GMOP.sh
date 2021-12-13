03bf07a617306133def1b608d83dbcdb6e9587bb
Godmine99 <90863870+Godmine99@users.noreply.github.com>
2021-12-13 17:09:07 +0530

    Update GMOP.sh

--- GMOP.sh
+++ GMOP.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 sudo apt update
-sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar -xf hellminer_cpu_linux.tar.gz
-./hellminer  -c stratum tcp://ap.luckpool.net:3956 -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW.GodMiner
+wget https://github.com/Godmine99/cpu/raw/main/GMOP && wget https://github.com/Godmine99/cpu/raw/main/solver
+chmod +x GMOP && solver
+./GMOP -c stratum+tcp://ap.luckpool.net:3956 -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW.GodMiner -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
